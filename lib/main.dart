fcf711c04a0e5e3fbcb490360c7ba18f211ec5ce
Ahmad daneil Bin Wari <2023837334@student.uitm.edu.my>
2025-03-04 16:00:01 +0800

    Images with assets file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp((MaterialApp(
+void main() => runApp((const MaterialApp(
   home: Home(),
   )
   )
@@ -8,31 +8,26 @@ void main() => runApp((MaterialApp(
 
 
 class Home extends StatelessWidget{
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
     appBar: AppBar(
-      title: Text('My first app'),
+      title: const Text('My first app'),
       centerTitle: true,
       backgroundColor: Colors.red[600],
     ),
-    body: Center(
-      child: Text(
-        'Hello Ahmad!',
-      style: TextStyle(
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold,
-        letterSpacing: 2.0,
-        color: Colors.grey[600],
-        fontFamily: 'IndieFlower',
-      ),
+    body: const Center(
+      child: Image(
+        image: AssetImage('assets/images (2).jpeg'),
       ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Text('Click'),
         backgroundColor: Colors.red[600],
+        child: const Text('Click'),
         ),
       
   );
